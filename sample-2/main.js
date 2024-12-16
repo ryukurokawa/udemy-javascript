ded1d3ea285b52bc450bb964c0dc240752d4b820
ryukurokawa <setsuna1228@gmail.com>
2024-12-16 17:32:32 +0900

    BMI計算

---
+++ sample-2/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#exec').addEventListener('click', function () {
+    const valueH = document.querySelector('#inputH').value;
+    const valueW = document.querySelector('#inputW').value;
+    const h = Number.parseInt(valueH) / 100; // メートルに直す
+    const w = Number.parseInt(valueW);
+    // BMIを計算する
+    let bmi = w / (h * h);
+    bmi = bmi.toFixed(2);
+
+    const html = `<div class="item">BMI <b>${bmi}</b></div>`;
+    document.querySelector('#output').innerHTML = html;
+  });
+});
